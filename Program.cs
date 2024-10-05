3c3de96f4b2c5a5e0b0e1f366a01568c185ec52f
dvinh3406 <pdv206@gmail.com>
2024-10-05 15:30:01 +0700

    add
    add

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int Scale = 23000;
+        Console.WriteLine("nhap vao so tien do ma ban co");
+        int usd = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("so tien viet ma ban co la{0}",Scale*usd);
+
+    }
+}
